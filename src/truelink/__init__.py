40e9d2916040175d1b417de259b4add5c1a96721
Mehedi Hasan Shojib <yesiamshojib@gmail.com>
2025-07-07 12:19:04 +0600

    Update __init__.py

--- src/truelink/__init__.py
+++ src/truelink/__init__.py
@@ -2,5 +2,5 @@ from .core import TrueLinkResolver
 from .exceptions import TrueLinkException, UnsupportedProviderException
 from .types import LinkResult, FolderResult
 
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 __all__ = ["TrueLinkResolver", "TrueLinkException", "UnsupportedProviderException", "LinkResult", "FolderResult"]
